eaad16039295f46672ba2db783820ba380086831
Typiespectre <63810422+Typiespectre@users.noreply.github.com>
2022-05-30 17:28:19 +0900

    Update bcd.sh

--- bcd.sh
+++ bcd.sh
@@ -11,7 +11,6 @@ function select_option {
   printf "$header %s\n" "$(pwd)"
   options=("$@")
  
-  # helpers for terminal print control and key input
   esc=$( printf "\033" )
   cursor_blink_on()   { printf "$esc[?25h"; }
   cursor_blink_off()  { printf "$esc[?25l"; }
@@ -21,19 +20,13 @@ function select_option {
   get_cursor_row()    { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
   key_input() {
     local key
-    # read 3 chars, 1 at a time
     for ((i=0; i < 3; ++i)); do
       read -s -n1 input 2>/dev/null >&2
-      # concatenate chars together
       key+="$input"
-      # if a number is encountered, echo it back
       if [[ $input =~ ^[1-9]$ ]]; then
         echo $input; return;
-      # if enter, early return
       elif [[ $input = "" ]]; then
         echo enter; return;
-      # if we encounter something other than [1-9] or "" or the escape sequence
-      # then consider it an invalid input and exit without echoing back
       elif [[ ! $input = $esc && i -eq 0 ]]; then
         return
       fi
@@ -56,20 +49,16 @@ function select_option {
     done
   }
 	
-  # initially print empty new lines (scroll down if at bottom of screen)
   for opt in "${options[@]}"; do printf "\n"; done
   
-  # determine current screen position for overwriting the options
   local lastrow=`get_cursor_row`
   local startrow=$(($lastrow - $#))
  
-  # ensure cursor and input echoing back on upon a ctrl+c during read -s
   trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
   cursor_blink_off
 
   local selected=$start_idx
   while true; do
-    # print options by overwriting the last lines
     local idx=0
     local label
     for opt in "${options[@]}"; do
@@ -83,25 +72,22 @@ function select_option {
       ((idx++))
     done
  
-    # user key control
     local input=$(key_input)
     case $input in
-		[1-9])
-		# '$#': number of parameters
-		  if [ $input -lt $(($# + 1)) ]; then
-        selected=$(($input - 1))
-					  break
-		  fi;;
+    [1-9])
+    if [ $input -lt $(($# + 1)) ]; then
+	selected=$(($input - 1))
+	break
+    fi;;
     enter) break;;
     esc) break;;
     up) ((selected--));
       if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
     down)  ((selected++));
       if [ $selected -ge $# ]; then selected=0; fi;;
-		esac
-	done
+    esac
+  done
  
-  # cursor position back to normal
   eraseMenu
   cursor_blink_on
  
